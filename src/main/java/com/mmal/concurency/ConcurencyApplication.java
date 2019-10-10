942f59f2808a65a021a94e5347ecd0d219e910ed
wxt <bit>
2019-10-10 10:19:08 +0800

    'init'

---
+++ src/main/java/com/mmal/concurency/ConcurencyApplication.java
@@ -0,0 +1,13 @@
+package com.mmal.concurency;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConcurencyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConcurencyApplication.class, args);
+	}
+
+}
